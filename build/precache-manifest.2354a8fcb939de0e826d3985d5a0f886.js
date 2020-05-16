f780141c89c850eddfd927fa51f8607bf95ef212
Mahima Chawla <mahima.chawla@classpass.com>
2020-05-16 00:03:09 -0600

    publish on github pages

---
+++ build/precache-manifest.2354a8fcb939de0e826d3985d5a0f886.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5219fa91f03dad7f66bb154efb5fdb54",
+    "url": "/react-web-app.git/index.html"
+  },
+  {
+    "revision": "020f601160e09773da81",
+    "url": "/react-web-app.git/static/css/main.0109b69b.chunk.css"
+  },
+  {
+    "revision": "eb4880153ba928dcc76d",
+    "url": "/react-web-app.git/static/js/2.eba4ac5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-web-app.git/static/js/2.eba4ac5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "020f601160e09773da81",
+    "url": "/react-web-app.git/static/js/main.5642c1b6.chunk.js"
+  },
+  {
+    "revision": "34a91866c20cfae94717",
+    "url": "/react-web-app.git/static/js/runtime-main.104e665f.js"
+  }
+]);
